b071f14e0453bc3dc5556ce446e27db428fbd344
Amelia Clarke <amelia.amok@gmail.com>
2021-04-06 09:14:21 -0500

    Add canvas

---
+++ src/main.js
@@ -0,0 +1,6 @@
+function setup() {
+    // Position the canvas.
+    const div = document.getElementById("sketch");
+    const canvas = createCanvas(div.offsetWidth, div.offsetHeight);
+    canvas.parent(div);
+}
